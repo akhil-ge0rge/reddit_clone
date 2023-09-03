5369a7958ffd5972df5f842cbfb37b1235139c6f
Akhil George <akhil8311@gmail.com>
2023-09-04 01:48:23 +0530

    home drawer

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:reddit_clone/features/auth/repository/auth_repository.dart';
+import 'package:reddit_clone/features/home/drawer/community_list_drawer.dart';
 
 import '../../auth/controller/auth_controller.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: false,
+        leading: Builder(builder: (ctx) {
+          return IconButton(
+            onPressed: () => displayDrawer(ctx),
+            icon: Icon(Icons.menu),
+          );
+        }),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+            ),
+          )
+        ],
+      ),
       body: Center(
         child: Text(user.name),
       ),
+      drawer: CommunityListDrawer(),
     );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screen/login_screen.dart';
+import 'package:reddit_clone/features/community/screen/create_community_screen.dart';
 import 'package:reddit_clone/features/home/screen/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
-final loggedInRoute =
-    RouteMap(routes: {'/': (_) => const MaterialPage(child: HomeScreen())});
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-community': (_) =>
+      const MaterialPage(child: CreateCommunityScreen()),
+});
 
-final loggedOutRoute =
-    RouteMap(routes: {'/': (_) => const MaterialPage(child: LoginScreen())});
+final loggedOutRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: LoginScreen()),
+});
